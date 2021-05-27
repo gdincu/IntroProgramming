fe8e8aa9523563c2037eca9545ac506871395e6d
NRO\I.G.DINCU <ionut.dincu@netrom.ro>
2021-05-27 10:03:58 +0300

    Add project files.

---
+++ IntroProgramming_Curs1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroProgramming_Curs1
+{
+    class Program
+    {
+        //Nullable type
+        public static void Metoda(int? tempInt)
+        {
+            Console.WriteLine(tempInt.HasValue ? tempInt.ToString() : "NULL");
+        }
+
+        //Optional parameter
+        public static void Metoda(int tempInt = 10)
+        {
+            Console.WriteLine(tempInt.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            Metoda(null);
+            Metoda(1);
+            Metoda();
+
+            sbyte tSbyte = sbyte.MaxValue;
+            byte tByte = byte.MinValue;
+ 
+            Console.ReadKey();
+
+        }
+    }
+}
